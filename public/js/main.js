b56e64d8d2fc405e7ea4adad926e68942582786e
Yossi Cohn <yossi.cohn@gmail.com>
2017-06-22 15:50:02 +0000

    add account support

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,94 @@
 $(document).ready(() => {
-  // Place JavaScript code here...
-  const userLocal = window.localStorage.getItem('public_userid');
-  if (userLocal != null && userLocal != undefined) {
-    window.public_userid = userLocal;
-  }
-
-  const loadJSResource = function (resourceURL, callback) {
-    if (resourceURL != null) {
-      const d = document;
-      const g = d.createElement('script');
-      const s = d.getElementsByTagName('script')[0];
-      g.type = 'text/javascript';
-      g.async = true;
-      g.defer = true;
-      g.src = resourceURL;
-      g.onload = callback;
-      s.parentNode.insertBefore(g, s);
+    // Place JavaScript code here...
+    const userLocal = window.localStorage.getItem('public_userid');
+    if (userLocal != null && userLocal != undefined) {
+        window.public_userid = userLocal;
     }
-  };
 
-  function onLoadSDK() {
-    console.log('called onSDKLoad');
+    const loadJSResource = function (resourceURL, callback) {
+        if (resourceURL != null) {
+            const d = document;
+            const g = d.createElement('script');
+            const s = d.getElementsByTagName('script')[0];
+            g.type = 'text/javascript';
+            g.async = true;
+            g.defer = true;
+            g.src = resourceURL;
+            g.onload = callback;
+            s.parentNode.insertBefore(g, s);
+        }
+    };
 
-    self.OptimoveSDKObj.initialize(null, self.OptimoveSDK.OptimoveSDKConfig, onSDKInitialized);
-  }
-  function onSDKInitialized(status) {
-    console.log(`testing my call back status = ${status}`);
+    function onLoadSDK() {
+        console.log('called onSDKLoad');
 
-    let url = self.document.location.href;
-    let host =  self.document.location.origin;
-    let splited =  url.split(host);
-    let pageName = splited[1];
-    let pageTitle  = pageName.slice(1, pageName.length);
-    if(pageTitle == "")
-        pageTitle = "Home - Hackathon Starter";
+        self.OptimoveSDKObj.initialize(null, self.OptimoveSDK.OptimoveSDKConfig, onSDKInitialized);
+    }
+    function onSDKInitialized(status) {
+        console.log(`testing my call back status = ${status}`);
 
-    self.OptimoveSDKObj.logPageVisitEvent('http://www.example.com' + pageName, pageTitle);
-  }
-  loadJSResource('https://optimovesdk.firebaseapp.com/sdk.js', onLoadSDK);
+        let url = self.document.location.href;
+        let host =  self.document.location.origin;
+        let splited =  url.split(host);
+        let pageName = splited[1];
+        let pageTitle  = pageName.slice(1, pageName.length);
+        if(pageTitle == "")
+            pageTitle = "Home - Hackathon Starter";
+
+        self.OptimoveSDKObj.logPageVisitEvent('http://www.example.com' + pageName, pageTitle);
+    }
+    loadJSResource('https://optimovesdk.firebaseapp.com/sdk.js', onLoadSDK);
+
+
+
+    function updateSignup(elem) {
+
+        var emailInputElement = document.getElementById('email');
+        var email = emailInputElement.value;
+        window.public_userid = email;
+        window.localStorage.setItem('public_userid', window.public_userid);
+        self.OptimoveSDKObj.setUserId(window.public_userid);
+
+
+        return false;
+    };
+    var loginButtonElem = document.getElementById('signup-button');
+    if(loginButtonElem != null)
+    {
+        loginButtonElem.onclick = updateSignup;
+    }
+
+    function updateLogin(elem) {
+
+        var emailInputElement = document.getElementById('email');
+        var email = emailInputElement.value;
+        window.public_userid = email;
+        window.localStorage.setItem('public_userid', window.public_userid);
+        self.OptimoveSDKObj.setUserId(window.public_userid);
+
+
+        return false;
+    }
+    var loginButtonElem = document.getElementById('login-button');
+    if(loginButtonElem != null)
+    {
+        loginButtonElem.onclick = updateLogin;
+    }
+
+
+    function updateLogout(elem) {
+
+        var emailInputElement = document.getElementById('email');
+        var email = emailInputElement.value;
+        window.public_userid = null;
+        window.localStorage.removeItem('public_userid');
+
+        return false;
+    }
+    var logoutButtonElem = document.getElementById('logout-link');
+    if(logoutButtonElem != null)
+    {
+        logoutButtonElem.onclick = updateLogout;
+    }
+
 });
-
