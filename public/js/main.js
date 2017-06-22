96da2430953b6ac13b593ae75ea892fa47753327
Yossi Cohn <yossi.cohn@gmail.com>
2017-06-22 11:44:24 +0000

    update site

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,42 @@
-$(document).ready(function() {
-
+$(document).ready(() => {
   // Place JavaScript code here...
-    var userLocal = window.localStorage.getItem('public_userid');
-    if (userLocal != null && userLocal != undefined) {
-        window.public_userid = userLocal;
-    };
+  const userLocal = window.localStorage.getItem('public_userid');
+  if (userLocal != null && userLocal != undefined) {
+    window.public_userid = userLocal;
+  }
 
-    var loadJSResource = function ( resourceURL, callback) {
+  const loadJSResource = function (resourceURL, callback) {
+    if (resourceURL != null) {
+      const d = document;
+      const g = d.createElement('script');
+      const s = d.getElementsByTagName('script')[0];
+      g.type = 'text/javascript';
+      g.async = true;
+      g.defer = true;
+      g.src = resourceURL;
+      g.onload = callback;
+      s.parentNode.insertBefore(g, s);
+    }
+  };
 
-        if(resourceURL != null)
-        {
-            var d = document;
-            var g = d.createElement('script');
-            var s = d.getElementsByTagName('script')[0];
-            g.type = 'text/javascript';
-            g.async = true;
-            g.defer = true;
-            g.src = resourceURL;
-            g.onload= callback ;
-            s.parentNode.insertBefore(g, s);
-        }
+  function onLoadSDK() {
+    console.log('called onSDKLoad');
 
+    self.OptimoveSDKObj.initialize(null, self.OptimoveSDK.OptimoveSDKConfig, onSDKInitialized);
+  }
+  function onSDKInitialized(status) {
+    console.log(`testing my call back status = ${status}`);
 
-    };
+    let url = self.document.location.href;
+    let host =  self.document.location.origin;
+    let splited =  url.split(host);
+    let pageName = splited[1];
+    let pageTitle  = pageName.slice(1, pageName.length);
+    if(pageTitle == "")
+        pageTitle = "Home - Hackathon Starter";
 
-    function onLoadSDK()
-    {
-        console.log('called onSDKLoad');
-        self.OptimoveSDKObj.initialize( null,  self.OptimoveSDK.OptimoveSDKConfig, onSDKInitialized );
-
-    };
-    function onSDKInitialized(status)
-    {
-        console.log('testing my call back status = ' + status);
-
-        self.OptimoveSDKObj.logPageVisitEvent('http://www.example.com', 'exmaple page');
-
-    };
-    loadJSResource('https://optimovesdk.firebaseapp.com/sdk.js', onLoadSDK);
+    self.OptimoveSDKObj.logPageVisitEvent('http://www.example.com' + pageName, pageTitle);
+  }
+  loadJSResource('https://optimovesdk.firebaseapp.com/sdk.js', onLoadSDK);
 });
+
