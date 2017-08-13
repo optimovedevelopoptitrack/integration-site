4c2753aee40f1c2f4e0b7d91abf1e9eae0a05240
MOBIUS\gil_g <gil_g@optimove.com>
2017-08-13 18:40:39 +0300

    aaa

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -1,23 +1,27 @@
 
-function setUserEmailEvent(){
-    var email = $("#userEmail").innerText;
-    optimoveSDK.API.reportEvent('Set_email_event', {'email': email} );    
-}
-$("#setUserEmail-btn").on("click",setUserEmailEvent)
 
-var action2 = document.getElementById('action2-btn');
-if(action2 != null)
-{
-    action2.addEventListener('click', updateAction );
-}
-var action3 = document.getElementById('action3-btn');
-if(action3 != null)
-{
-    action3.addEventListener('click', updateAction );
-}
+$(document).ready(function(){
+    function setUserEmailEvent(){
+        var email = $("#userEmail").innerText;
+        optimoveSDK.API.reportEvent('Set_email_event', {'email': email} );    
+    }
+    $("#setUserEmail-btn").on("click",setUserEmailEvent)
+    
+    var action2 = document.getElementById('action2-btn');
+    if(action2 != null)
+    {
+        action2.addEventListener('click', updateAction );
+    }
+    var action3 = document.getElementById('action3-btn');
+    if(action3 != null)
+    {
+        action3.addEventListener('click', updateAction );
+    }
+    
+    var action4 = document.getElementById('action4-btn');
+    if(action4 != null)
+    {
+        action4.addEventListener('click', updateAction );
+    }
+})
 
-var action4 = document.getElementById('action4-btn');
-if(action4 != null)
-{
-    action4.addEventListener('click', updateAction );
-}
