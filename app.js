c3b3507f1a4c171c7b275175e69eab53c17b49ca
MOBIUS\gil_g <gil_g@optimove.com>
2017-08-13 17:35:56 +0300

    add realtime controller

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
-
+const realtimeController = require('./controllers/realtime');
 /**
  * API keys and Passport configuration.
  */
